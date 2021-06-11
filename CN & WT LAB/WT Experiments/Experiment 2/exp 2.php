eee15515c7123f4ea97f2fcdd24bf32813f6e0e3
18wh1a1240 <53773724+18wh1a1240@users.noreply.github.com>
2021-06-11 11:51:42 +0530

    Add files via upload

---
+++ CN & WT LAB/WT Experiments/Experiment 2/exp 2.php
@@ -0,0 +1,21 @@
+1.	
+<?php 
+    $str = "Anuhya Javvaji"; 
+    echo strlen($str); 
+?>  
+2.
+<?php
+echo str_word_count("Anuhya Javvaji from 3rd IT");
+?>
+
+3.
+<?php
+echo strrev("Anuhya Javvaji from 3rd IT");
+?>
+
+4.
+<?php
+if (strpos('A b c d', 'b')) {
+    echo "Checking the existence of the empty string will always return true";
+}
+?>
