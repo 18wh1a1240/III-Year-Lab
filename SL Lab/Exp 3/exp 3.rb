e3553e9517782510d13350093ef0987ed5c11dc0
18wh1a1240 <53773724+18wh1a1240@users.noreply.github.com>
2021-07-07 10:59:10 +0530

    Add files via upload

---
+++ SL Lab/Exp 3/exp 3.rb
@@ -0,0 +1,8 @@
+puts "Enter first name: "
+str1 = gets.chomp
+
+puts "Enter second name: "
+str2 = gets.chomp
+
+puts "Actual Name #{str2} #{str1}"
+puts "Reverse Name #{str2.reverse} #{str1.reverse}"
