6030d51d1d06bdc057e307462921c88478304bf7
18wh1a1240 <53773724+18wh1a1240@users.noreply.github.com>
2021-07-07 10:32:46 +0530

    Add files via upload

---
+++ SL Lab/Exp 2/exp 2.rb
@@ -0,0 +1,10 @@
+puts "Enter the radius of circle: "
+
+r = gets.chomp.to_f
+perimeter = 0.0
+area = 0.0
+perimeter = 2 * 3.14 * r
+area = 3.14 * r * r
+
+puts "The perimeter of a circle is #{perimeter}"
+puts "The area of a circle is #{area}"
